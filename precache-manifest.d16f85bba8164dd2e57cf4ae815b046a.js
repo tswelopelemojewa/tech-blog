19efe69b1ae13301d57ed1e160253387d1036cf7
Tswelopele Mojewa <tswelopelemojewa@gmail.com>
2022-07-28 00:26:51 +0200

    Updates

---
+++ precache-manifest.d16f85bba8164dd2e57cf4ae815b046a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c17bd5325a296b211da4",
+    "url": "/tech-blog/static/css/main.58226cd3.chunk.css"
+  },
+  {
+    "revision": "c17bd5325a296b211da4",
+    "url": "/tech-blog/static/js/main.ce8aeae4.chunk.js"
+  },
+  {
+    "revision": "7032f03d263e59e46b1a",
+    "url": "/tech-blog/static/js/runtime~main.9bc59408.js"
+  },
+  {
+    "revision": "d17c3a0dd241165c736b",
+    "url": "/tech-blog/static/js/2.735aa09e.chunk.js"
+  },
+  {
+    "revision": "5275c8b554ba410b2ced43e462c6ab50",
+    "url": "/tech-blog/index.html"
+  }
+];
